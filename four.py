3bd3f3a83a1f502120e731cebf961f2485723c6c
Anmol Dwivedi <anmoldwivedi@Anmols-MacBook.local>
2026-06-03 16:31:04 +0530

    Completed Day 4 Python

---
+++ four.py
@@ -0,0 +1,107 @@
+#  List
+# Create a list of 5 numbers and print them.
+
+# numbers = [10, 20, 30, 40, 50]
+
+# print(numbers)
+
+# Find largest element in a list
+# largest = max(numbers)
+
+# print("largest number is :", largest)
+
+# Find sum of all elements
+# total = sum(numbers)
+
+# print("Total is :", total)
+
+# Count even numbers in a list
+
+# count = 0 
+
+# for num in numbers:
+#     if num % 2 == 0:
+#         count += 1
+
+# print("Number of even elements :", count)
+
+# Reverse a list 
+
+# numbers.reverse()
+
+# print(numbers)
+
+# Tuples
+# Create a tuple of 5 fruits
+
+# fruits = ("apple", "banana", "orange", "grapes", "watermelon")
+
+# print(fruits)
+
+# Print first and last element
+
+# print("first element :",fruits[0])
+# print("second element :",fruits[-1])
+
+# Count how many times a value appears
+
+# count = fruits.count("apple")
+
+# print("apple apppears", count, "time")
+
+# Sets
+# Create a set with duplicate values
+
+numbers = {10, 20, 30, 20, 40, 10, 50}
+
+# print(numbers)
+
+# Add a new value
+
+# numbers.add(70)
+
+# print(numbers)
+
+# Remove a value
+
+# numbers.remove(30)
+
+# print(numbers)
+
+# Print unique elements from a list using a set
+
+# unique_numbers = set(numbers)
+
+# print(unique_numbers)
+
+# Dictionaries 
+# Store Student details
+
+student = {
+    "name": "Anmol",
+    "age": 22,
+    "course": "B.Tech",
+    "city": "Banda"
+}
+
+# print(student)
+
+# Print all keys
+
+# print(student.keys())
+
+# Print all values
+
+# print(student.values())
+
+# Update age
+
+# student["age"] = 21
+
+# print(student)
+
+# Add a new field like "college"
+
+# student["college"] = "ITM University"
+
+# print(student)
---
+++ third.py
@@ -0,0 +1,97 @@
+
+# area of circle function
+
+# def area_of_circle(radius):
+#     area = 3.14 * radius * radius
+#     return area
+
+# r = float(input("enter the radius :"))
+
+# result = area_of_circle(r)
+
+# print ("area of circle  =", result)
+
+# Function to check Prime number
+
+# def is_prime(num):
+    
+#     if num <= 1:
+#        return False
+
+#     for i in range(2,num):
+#         if num % i == 0:
+#            return False 
+    
+#     return True  
+
+# number = int(input("Enter a number "))
+
+# if is_prime:
+#     print("Prime number")
+# else:
+#     print("Not a Prime number")
+
+# Function to reverse a number 
+
+# def reverse_number(num):
+
+#     reverse = 0
+
+#     while num > 0:
+#           digit = num % 10 
+#           reverse = reverse * 10 + digit
+#           num = num // 10
+          
+#     return reverse 
+    
+# number = int(input("Enter a number :"))
+
+# result = reverse_number(number)
+
+# print ("Reversed number =", result)
+
+# Function to count digits
+
+# def  count_number(num):
+
+#     count = 0
+
+#     while num > 0:
+#         count += 1
+#         num = num // 10
+
+#     return count
+
+# number = int (input("Enter a number :"))
+
+# result = count_number(number)
+
+# print ("Number of digits :", result)
+
+#  Function to calculate average
+
+# def calculate_average(marks1 , marks2, marks3):
+    
+#     Total = marks1 + marks2 + marks3 
+#     average = Total / 3
+
+#     return average 
+
+# m1 = float(input("Enter marks of sub1 :"))
+# m2 = float(input("Enter marks of sub2 :"))
+# m3 = float(input("Enter marks of sub3 :"))
+
+# result = calculate_average(m1, m2, m3)
+
+# print ("average is :", result)
+
+# Function to print multiplication table
+
+# def multiplication_table(num):
+
+#     for i in range(1, 11):
+#      print( num, "x", i, "=", num*i)
+
+# number = int(input("Enter a number :"))
+
+# multiplication_table(number)
