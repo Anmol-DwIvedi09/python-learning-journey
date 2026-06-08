67512228236589354d9ec154501badfa73e7bbf8
Anmol Dwivedi <anmoldwivedi292@gmail.com>
2026-06-08 17:26:02 +0530

    Day 7: OOP basics - classes and objects

---
+++ day7_Oops_basics.py
@@ -0,0 +1,103 @@
+# Create a Car class
+
+# class car :
+#     def __init__(self, brand):
+#         self.brand = brand
+
+# my_car = car("Bmw M5")
+# print(my_car.brand)
+
+# # create a student class 
+
+# class student :
+#     def __init__(self, name, age):
+#      self.name = name 
+#      self.age = age 
+
+# student1 = student("Anmol", 22)
+# student2 = student("Dheeraj", 22)
+
+# print("Name :",student1.name)
+# print("age :", student1.age)
+# print("Name :",student2.name)
+# print("age :", student2.age)
+
+# Create a Person class
+
+# class person :
+#     def greet(self):
+#         print("Hello")
+
+# person1 = person()
+
+# person1.greet()
+
+# Create a Rectangle class
+
+# class rectangle:
+#     def __init__(self, length, breadth):
+#         self.length = length 
+#         self.breadth = breadth
+
+#     def area (self):
+#         return self.length * self.breadth 
+    
+# rect = rectangle(12 , 8)
+
+# print("Area :", rect.area())
+
+# Create a BankAccount class
+
+# class BankAccount :
+
+#     def __init__(self, account_holder, balance):
+#         self.account_holder = account_holder
+#         self.balance = balance 
+
+#     def show_balance (self):
+#         print("Account Holder :", self.account_holder)
+#         print("Balance :", self.balance)
+
+# account = BankAccount("Anmol", 50000)
+
+# account.show_balance()
+
+# Create a Book class
+
+# class Bookclass :
+#     def __init__(self, title, author):
+#         self.title = title
+#         self.author = author 
+
+#     def display_book(self):
+#      print("Title :", self.title)
+#      print("Author :", self.author)
+
+# book = Bookclass("Skyfire", "Aroon raman")
+
+# book.display_book()
+
+# Mini Assignment 
+
+# class Student:
+#     def __init__(self, name , age):
+#         self.name = name
+#         self.age = age 
+
+#     def display(self):
+#         print("Name :", self.name)
+#         print("Age :", self.age)
+#         print("-" * 20)
+
+# student1 = Student("Anmol", 21)
+# student2 = Student("Mohit", 22)
+# student3 = Student("Dheeraj", 23)
+
+# student1.display()
+# student2.display()
+# student3.display()
+
+
+
+
+
