12ec188456e5f9823a090e673aa67f6bf894f9a4
Anmol Dwivedi <anmoldwivedi292@gmail.com>
2026-07-02 10:23:31 +0530

    Add Day 10 NumPy fundamentals

---
+++ day10_numpy_Fundamentals.py
@@ -0,0 +1,58 @@
+# create an array 
+
+# import numpy as np
+
+# arr = np.array([5,10,15,20,25])
+
+# print("array :", arr)
+# print("shape :", arr.shape)
+# print("Dimensions :", arr.ndim)
+
+
+# Create a 3×3 matrix filled with zeros
+
+# import numpy as np
+
+# matrix = np.zeros((3,3))
+
+# print(matrix)
+
+# Create numbers from 1–20 using arange()
+
+# import numpy as np
+
+# matrix = np.arange(1,21) 
+
+# print(matrix)
+
+# Find Mean, Median, Standard Deviation 
+
+# import numpy as np
+
+# arr = np.array([10,20,30,40,50])
+
+# print("Mean :", np.mean(arr))
+# print("Median :", np.median(arr))
+# print("Standard Deviation :", np.std(arr))
+
+# Reshaping array
+
+# import numpy as np
+
+# arr = np.array([1,2,3,4,5,6])
+
+# new_arr = arr.reshape(2,3)
+
+# print("New array :", new_arr)
+
+# Students Marks analyzer
+
+# import numpy as np 
+
+# marks = np.array([85,90,78,92,88,76,95])
+
+# print("Highest marks :", np.max(marks))
+# print("Lowest marks :", np.min(marks))
+# print("Average marks :", np.mean(marks))
+# print("Standard Deviation :", np.std(marks))
+# print("Total marks :", np.sum(marks))
